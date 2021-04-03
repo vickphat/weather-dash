17ef79998481ff1043f0640e267417fbdb857ea5
Vicheka Phat <vickphat@gmail.com>
2021-04-03 10:00:50 -0400

    fixed fetch call

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,20 +6,30 @@ var currentTemp = $("#temperature");
 var currentHumidity = $("#humidity");
 var currentWindSpeed = $("#wind-speed");
 var currentUV = $("#UV-index");
-var enterCity = $("#enter-city")
+var enterCity = $("#enter-city");
+var city = "";
+
+function weather(event) {
+    event.preventDefault();
+    if (enterCity.val().trim() !== "") {
+        city = enterCity.val().trim();
+        currentWeather(city);
+    }
+}
 
 searchButton.click(weather);
 
 
-function weather(apiURL) {
-
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + enterCity + "&units=imperial&appid=37fc43925c5a51f9ab5bea46e987dda6&lang=en";
+function currentWeather(city) {
+    console.log(city)
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=37fc43925c5a51f9ab5bea46e987dda6&lang=en";
+    console.log(apiURL)
 
     fetch(apiURL)
         .then(function (response) {
-            response.json()
+            return response.json()
             .then(function (data) {
-                console.log(this)
+               
 
             });
         });
